9d45f65a237955c76aed4ced2353f483c4cf8a49
Loginova Irina <Irina_Loginova1@epam.com>
2022-05-30 09:56:24 +0200

    Final Task- Add function App for OrderItemsReceiver

---
+++ src/eShopFunctionApp/OrderItemsReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace eShopFunctionApp
+{
+    public static class OrderItemsReceiver
+    {
+        [FunctionName("OrderItemsReceiver")]
+        public static void Run(
+            [ServiceBusTrigger("shoporders", Connection = "ServiceBusConn")]
+            string myQueueItem,
+            [Blob("orders/{rand-guid}", FileAccess.Write, Connection = "BlobOrderConn")] out string orders,
+            ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try 
+            {
+                log.LogInformation(myQueueItem);
+                orders = myQueueItem;
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.ToString());
+                throw ex;
+            }
+        }
+    }
+}
